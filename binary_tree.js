91ca037b3ea23b38c11a11d3733c86b862ac2dc6
eschoppik <eschoppik@gmail.com>
2014-12-03 21:02:27 -0800

    fixing tests

--- binary_tree.js
+++ binary_tree.js
@@ -4,10 +4,41 @@ function BinTree(value){
   this.value = value;
 };
 
+BinTree.prototype.insert = function(value){
+  if(value < this.value){
+    if(this.left === null){
+      this.left = new BinTree(value);
+    } else {
+      this.left.insert(value)
+    }
+  } else {
+    if(this.right === null){
+      this.right = new BinTree(value)
+    } else {
+      this.right.insert(value);
+    }
+  }
+  return this;
+};
 
-BinTree.prototype.max = function(){
+BinTree.prototype.isLeaf = function(){
+  return this.left === null && this.right === null;
+};
 
-}
+BinTree.prototype.search = function(value){
+  if (this.value == value){
+    return true
+  } else if (this.isLeaf()){
+    return false;
+  } else if (value < this.value){
+    return this.left.search(value);
+  } else {
+    return this.right.search(value);
+  }
+};
+
+console.log(BinTree.prototype.isLeaf)
+
 try{
   module.exports = BinTree;
 } catch(e){
